c502e7b0a2f5ebe71839c4454f704f0c1aa4e12c
Matrix-Zat <godswaydoe761@gmail.com>
2025-05-08 18:07:55 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,81 @@
+//-------------------[ BOT SETTINGS ]------------------// 
+
+// @project_name : MATRIX-X-KING
+// @author : MATRIX
+// @telegram : http://t.me/MatriXXXXXXXXX
+// @github : Matri199
+// @whatsapp : +23593734312
+
+//----------------------[ MATRIX-KING ]----------------------//
+
+const fs = require('fs')
+const { color } = require('./lib/color')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+
+
+//--------------------[ SESSION ID ]----------------------//
+
+global.SESSION_ID = process.env.SESSION_ID || '' 
+//Enter your Xploader session id here; must start with XPLOADER-BOT:~
+
+//--------------------[ BOT NAME ]----------------------//
+
+global.botname = process.env.BOT_NAME || 'Queen-Adiza' 
+
+//-----------------[ OWNER NUMBER ]------------------//
+
+global.ownernumber = process.env.OWNER_NUMBER || '233593734312' 
+
+//--------------------[ SUDO ]--------------------------//
+
+global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['254796180105', '254712345678'];
+// Type additional allowed users here
+//NB: They'll be able to use every functions of the bot without restrictions.
+
+//-----------------[ OWNER NAME ]------------------//
+
+global.ownername = process.env.OWNER_NAME || 'Matrix-X-King' 
+
+//------------[ STICKER PACKNAME ]-----------------//
+
+global.packname = process.env.STICKER_PACK_NAME || "Queen-Adiza" 
+
+//--------------[ STICKER AUTHOR NAME ]------------//
+
+global.author = process.env.STICKER_AUTHOR_NAME || "Adizatu" 
+
+//----------------[ GITHUB DATABASE ]-----------------//
+
+global.dbToken = process.env.GITHUB_TOKEN || "";
+
+
+//-----------------[ CONTEXT LINK ]--------------------//
+
+global.plink = process.env.PLINK || "https://www.instagram.com/heyits_matrix?igsh=YzljYTk1ODg3Zg---"
+
+//------------------[ WATERMARK ]--------------------//
+
+global.wm = process.env.GL_WM || "©Matrix"
+
+//---------------------[ REPLIES ]-----------------------//
+
+global.mess = { 
+  done: '*Done*', 
+  success: '©Matrix', 
+  owner: `*You don't have permission to use this command!*`, 
+  group: '*This feature becomes available when you use it in a group!*', 
+  admin: '*You’ll unlock this feature with me as an admin!*', 
+  notadmin: '*This feature will work once you become an admin. A way of ensuring order!*' 
+}
+
+//--------------------[ WATCHER ]-----------------------//
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(color(`Updated '${__filename}'`, 'red'))
+  delete require.cache[file]
+  require(file)
+})
+
+//----------------------[ MATRIX-X ]----------------------//
